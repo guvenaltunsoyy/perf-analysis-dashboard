13c873a2793e72b66e1ba0f75f22b2669cded3b5
Güven ALTUNSOY <guven@shopihq.com>
2021-07-12 22:19:35 +0300

    env local file added

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const dev = {
-    BASE_API_URL: "http://localhost:8080/v1"
-}
-
-const prod = {
-    BASE_API_URL: "llll"
-}
-const config = process.env.NODE_ENV === 'development' ? dev : prod
-export default config
