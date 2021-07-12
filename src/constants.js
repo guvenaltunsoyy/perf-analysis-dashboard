dc4c9654137e8c980ec36aee87c0e5aa1265c89f
Güven ALTUNSOY <guven@shopihq.com>
2021-07-12 21:52:49 +0300

    log added to constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 const BASE_URI = process.env.NODE_ENV !== "production" ? 'http://localhost:8080/v1/' : process.env.BASE_API_URL;
+console.log(process.env)
 export const GET_TTFB_URI = `${BASE_URI}ttfb`
 export const GET_RESOURCES_URI = `${BASE_URI}resources`
 export const GET_PAINTS_URI = `${BASE_URI}paints`
