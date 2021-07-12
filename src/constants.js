0b345023435c3401af4bc4092c5fd51225acd729
Güven ALTUNSOY <guven@shopihq.com>
2021-07-12 22:42:10 +0300

    env local fix

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const BASE_URI = process.env.NODE_ENV !== "production" ? 'http://localhost:8080/v1/' : process.env.BASE_API_URL;
+const BASE_URI = process.env.REACT_APP_BASE_API_URL;
 console.log(process.env)
 export const GET_TTFB_URI = `${BASE_URI}ttfb`
 export const GET_RESOURCES_URI = `${BASE_URI}resources`
